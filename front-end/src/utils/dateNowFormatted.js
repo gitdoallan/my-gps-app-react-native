047693661a948948c8847f25e11dc7369b0dfaf7
gitdoallan <mailtoallan@icloud.com>
2022-09-26 08:38:54 -0300

    feat: adding hh:mm to date

--- front-end/src/utils/dateNowFormatted.js
+++ front-end/src/utils/dateNowFormatted.js
@@ -3,5 +3,7 @@ export const dateNowFormatted = () => {
   const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
   const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
   const year = date.getFullYear();
-  return `${year}-${month}-${day}`;
+  const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+  const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
 };
